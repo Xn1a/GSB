71e9b6fd4d3b32993eeab42378f207f319db5df4
Nia <xn1a@protonmail.com>
2018-11-16 12:38:25 +0100

    Affichage des frais forfait et modification

--- controleurs/c_validerFiches.php
+++ controleurs/c_validerFiches.php
@@ -15,38 +15,78 @@
  * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
  */
 
+$fonctionUtilisateur = $_SESSION['fonction'];
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 
-// Récupération de la liste des visiteurs
+// Action par defaut "selectionnerVisiteur" : récupération de la liste
+// des visiteurs et du visiteur selectionné si il y en a
+$idVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
+$idVisiteurASelectionner = $idVisiteur;
 $lesVisiteurs = $pdo->getLesVisiteurs();
-
-$idVisiteur = null;
-$fiche = null;
-
-if ($action == "selectionnerMois") {
-    // On récupère l'id de l'utilisateur selectionné
-    $idVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
-    // On le reselection dans la liste
-    $idVisiteurASelectionner = $idVisiteur;
-}
-if ($action == "corrigerFrais") {
-    $idVisiteur = filter_input(INPUT_POST, 'idVisiteur', FILTER_SANITIZE_STRING);
-    $idVisiteurASelectionner = $idVisiteur;
-    $fiche = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
-    $moisASelectionner = $fiche;
-}
 require 'vues/v_listeVisiteurs.php';
 
-if ($action == "selectionnerMois") {
-    $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
-    include 'vues/v_listeMoisComptables.php';
+// Récupération du mois selectionné si il y en a
+$fiche = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+if (isset($fiche)) {
+    $moisASelectionner = $fiche;
 }
-// Fiche selectionnée : on affiche les formulaires
-if ($action == "corrigerFrais") {
-    // Réaffiche le mois selectionné
-    $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
-    include 'vues/v_listeMoisComptables.php';
 
-    // Affiche les formulaires
-    // TODO : include les formulaire d'affichage et correction de la fiche
+switch ($action) {
+    case 'selectionnerMois':
+        // Affiche la liste des mois
+        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+        include 'vues/v_listeMoisComptables.php';
+        break;
+
+    case 'corrigerFrais':
+        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+        include 'vues/v_listeMoisComptables.php';
+
+        // Affiche les formulaires
+        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $fiche);
+
+        // Affichage en fonction de la fonction de l'utilisateur
+        // Si Visiteur
+        $texteSubmit = "Ajouter";
+        $texteReset = "Effacer";
+        $titre = 'Renseigner la fiche de frais du mois' . $numMois . '-' . $numAnnee;
+        $actionFormulaire = "index.php?uc=gererFrais&action=validerMajFraisForfait";
+        // Si comptable
+        if ($fonctionUtilisateur == "Comptable") {
+            $texteSubmit = "Corriger";
+            $texteReset = "Réinitialiser";
+            $titre = "Valider la fiche de frais";
+            $actionFormulaire = "index.php?uc=validerFiches&action=validerMajFraisForfait";
+        }
+
+        include 'vues/v_listeFraisForfait.php';
+        echo "<hr/>";
+        // TODO : include les formulaire d'affichage et correction de la fiche
+
+        break;
+
+    case 'validerMajFraisForfait':
+        $lesFrais = filter_input(INPUT_POST, 'lesFrais', FILTER_DEFAULT, FILTER_FORCE_ARRAY);
+
+        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+        include 'vues/v_listeMoisComptables.php';
+
+        if (lesQteFraisValides($lesFrais)) {
+            $pdo->majFraisForfait($idVisiteur, $fiche, $lesFrais);
+        } else {
+            ajouterErreur('Les valeurs des frais doivent être numériques');
+            include 'vues/v_erreurs.php';
+        }
+
+        // Affichage des formulaires
+        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $fiche);
+
+        // Pour comptable
+        $texteSubmit = "Corriger";
+        $texteReset = "Réinitialiser";
+        $titre = "Valider la fiche de frais";
+        $actionFormulaire = "index.php?uc=validerFiches&action=validerMajFraisForfait";
+        include 'vues/v_listeFraisForfait.php';
+        echo "<hr/>";
+        break;
 }
